ad156ca16d3d89bdc4e333089701a6ad91e2af3e
AnahitAmirkhanyan <anahit.amirkhanyan.dev@gmail.com>
2021-04-30 18:16:37 +0400

    components separated

--- src/index.js
+++ src/index.js
@@ -20,15 +20,18 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    const squares = [...this.state.squares];
+    console.log(squares)
     if(calculateWinner(squares) || squares[i]) {
       return;
     }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+
     this.setState({
-      squares: squares,
-    xIsNext: !this.state.xIsNext,
-   });
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -107,6 +110,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
