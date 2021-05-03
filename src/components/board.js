338044696976d861802dae8ae7d2334278c9eafe
AnahitAmirkhanyan <anahit.amirkhanyan.dev@gmail.com>
2021-05-03 20:33:45 +0400

    better

--- src/components/board.js
+++ src/components/board.js
@@ -13,7 +13,7 @@ class Board extends React.Component {
   
     handleClick(i) {
       const squares = [...this.state.squares];
-      
+
       if(squares[i]){
         return;
       }
@@ -75,109 +75,87 @@ class Board extends React.Component {
   }
 
   function calculateWinner(squares, n) {
-    // const lines = [
-    //   [0, 1, 2],
-    //   [3, 4, 5],
-    //   [6, 7, 8],
-    //   [0, 3, 6],
-    //   [1, 4, 7],
-    //   [2, 5, 8],
-    //   [0, 4, 8],
-    //   [2, 4, 6],
-    // ];
-  
-    // for (let i = 0; i < lines.length; i++) {
-    //   const [a, b, c] = lines[i];
-      
-    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-    //     return squares[a];
-    //   }
-    // }
-    // return null;
-
     const newArr = [];
     const squaresCopy = [...squares];
     while(squaresCopy.length){
       newArr.push(squaresCopy.splice(0,3));
     } 
+    let winner = null;
     
-    let count = 0;
     // check rows
     let currentRow = Math.floor(n / 3);
+    winner = checkWinner(checkRows(newArr, currentRow));
+    if(winner) return winner;
+
+    // check columns
+    let currentCol = n % 3;
+    winner = checkWinner(checkColumns(newArr, currentCol));
+    if(winner) return winner;
+
+
+    // check diag1 if square is on diag1
+    if(currentRow === currentCol){
+      winner = checkWinner(checkDiag1(newArr));
+      if(winner) return winner;
+    }
+
+    // check diag2 if square is on diag2
+    if(currentRow + currentCol === 2){
+      winner = checkWinner(checkDiag2(newArr));
+      if(winner) return winner;
+    }
+
+    return null;
+  }
+
+  function checkWinner(count){
+    if(count === 3) return 'X';
+    if(count === -3) return 'O';
+    return null;
+  }
+
+  function checkRows(newArr, currentRow){
+    let count = 0;
     for(let i = 0; i < 3; i++){
       if(newArr[currentRow][i] == null) break;
 
       if(newArr[currentRow][i] === 'X') count++;
       else count--;
     }
+    return count;
+  }
 
-    if(count === 3){
-      return 'X';
-    }
-
-    if(count === -3){
-      return 'O';
-    }
-
-    // check columns
-    count = 0;
-    let currentCol = n % 3;
+  function checkColumns(newArr, currentCol){
+    let count = 0;
     for(let i = 0; i < 3; i++) {
       if(newArr[i][currentCol] == null) break;
 
       if(newArr[i][currentCol] === 'X') count++;
       else count--;
-
-      console.log(count);
     }
+    return count;
+  }
 
-    if(count === 3){
-      return 'X';
+  function checkDiag1(newArr){
+    let count = 0;
+    for(let i = 0; i < 3; i++){
+      if(newArr[i][i] == null) break;
+
+      if(newArr[i][i] === 'X') count++;
+      else count--;
     }
+    return count;
+  }
 
-    if(count === -3){
-      return 'O';
+  function checkDiag2(newArr){
+    let count = 0;
+    for(let i = 0; i < 3; i++){
+      if(newArr[i][2 - i] == null) break;
+
+      if(newArr[i][2 - i] === 'X') count++;
+      else count--;
     }
-
-    // check diag1 if square is on diag1
-    count = 0;
-    if(currentRow === currentCol){
-      for(let i = 0; i < 3; i++){
-        if(newArr[i][i] == null) break;
-
-        if(newArr[i][i] === 'X') count++;
-        else count--;
-      }
-    }
-
-    if(count === 3){
-      return 'X';
-    }
-
-    if(count === -3){
-      return 'O';
-    }
-
-    // check diag2 if square is on diag2
-    count = 0;
-    if(currentRow + currentCol === 2){
-      for(let i = 0; i < 3; i++){
-        if(newArr[i][2 - i] == null) break;
-
-        if(newArr[i][2 - i] === 'X') count++;
-        else count--;
-      }
-    }
-
-    if(count === 3){
-      return 'X';
-    }
-
-    if(count === -3){
-      return 'O';
-    }
-
-    return null;
+    return count;
   }
 
 
